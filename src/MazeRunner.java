c2f596e45eaab2b261b91bdac3b9e1a09c978127
Thenuja <liyanage.thenuja@gmail.com>
2020-07-29 22:16:54 -0700

    Third Commit

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,30 +1,54 @@
 import java.util.*;
 
-//Countinue for Part 1, number 6: moving the character.
+//Start from part 2; making a counter.
 public class MazeRunner {
-    public Maze myMap = new Maze();
+    static Maze myMap = new Maze();
+
     public static void main(String[] args){
 
         intro();
-        userMove();
+        String movement = userMove();
+        System.out.println(movement);
+
     }
 
     public  static void intro(){
-        Maze myMap = new Maze();
-
         System.out.println("Welcome to Maze Runner!\nHere is your current position:");
         myMap.printMap();
 
     }
 
-    public static void userMove(){
-        Scanner input = new Scanner(System.in);
+    public static String userMove(){
+        String direction = "";
         System.out.println("Where would you like to move? (R, L, U, D)");
-        String direction = input.next();
+        while (myMap.didIWin() != true) {
+            while (!direction.equals("R") && !direction.equals("L") && !direction.equals("U") && !direction.equals("D")) {
 
-            if (!direction.equals("R") && !direction.equals("L") && !direction.equals("U") && !direction.equals("D")){
-                System.out.println("Please only enter one of these value: R, L, U, D");
-                userMove();
+                Scanner input = new Scanner(System.in);
+                direction = input.next();
+
+                if (!direction.equals("R") && !direction.equals("L") && !direction.equals("U") && !direction.equals("D")) {
+                    System.out.println("Please only enter one of these values: R, L, U, D");
+                    direction = "";
+                }
             }
+            if (direction.equals("R") && myMap.canIMoveRight()) {
+                myMap.moveRight();
+            } else if (direction.equals("L") && myMap.canIMoveLeft()) {
+                myMap.moveLeft();
+            } else if (direction.equals("U") && myMap.canIMoveUp()) {
+                myMap.moveUp();
+            } else if (direction.equals("D") && myMap.canIMoveDown()) {
+                myMap.moveDown();
+            } else {
+                System.out.println("Sorry, you’ve hit a wall.");
+            }
+
+            myMap.printMap();
+            System.out.println("Where would you like to move next: R  L  U  D");
+            direction = "";
+        }
+        System.out.println("Congratulations! You've made it out alive!");
+            return direction;
     }
 }
